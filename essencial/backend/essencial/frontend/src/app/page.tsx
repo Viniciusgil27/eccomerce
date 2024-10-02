8a323827e94ef208d9dde9489915b79ab8f4a336
Sousa_viny <viny.gil27@gmail.com>
2024-10-01 23:26:42 -0300

    segundo dia

---
+++ essencial/backend/essencial/frontend/src/app/page.tsx
@@ -0,0 +1,13 @@
+import ProdutoItem from "@/components/produto/ProdutoItem";
+import { produtos } from "@/core";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="grid grid-cols-4 gap-5 container">
+      {produtos.map((produto) => (
+        <ProdutoItem key={produto.id} produto={produto}/>
+      ))}
+    </div> 
+  );
+}
